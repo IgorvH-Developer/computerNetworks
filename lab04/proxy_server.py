197ce4b66aff5da7ff07dab4998d27dd004a83db
hlopin <hlopin@protei.ru>
2025-03-14 10:21:57 +0300

    Добавлено ДЗ 4

--- lab04/proxy_server.py
+++ lab04/proxy_server.py
@@ -35,7 +35,19 @@ def get_response(server_socket):
 
     return response
 
-def transformRequest(request, host, method, url, protocol):
+def send_block_message(client_socket):
+    block_message = (
+        f"HTTP/1.1 403 Forbidden\r\n"
+        f"Content-Type: text/html; charset=utf-8\r\n"
+        f"Connection: close\r\n"
+        f"\r\n"
+        f"<html><body><h1>Доступ запрещен!</h1>"
+        f"<p>Эта страница в чёрном списке.</p></body></html>\r\n"
+    )
+    client_socket.send(block_message.encode('utf-8'))
+    client_socket.close()
+
+def transform_request(request, host, method, url, protocol):
     headers = request.decode().split("\r\n\r\n")[0]
     header_dict = parse_headers(headers)
 
@@ -55,7 +67,6 @@ def transformRequest(request, host, method, url, protocol):
 def proxy_server(client_socket, addr):
     request = client_socket.recv(4096)
 
-    # Парсим запрос
     first_line = request.decode().split('\n')[0]
     method, path, protocol = first_line.split()
     parsed_url = urlparse(path)
@@ -67,17 +78,24 @@ def proxy_server(client_socket, addr):
         params = parse_qs(parsed_url.query)
         host = params['host'][0]
         url = host
+    new_request, new_host = transform_request(request, host, "GET", url, protocol)
+
+    blacklist_file = "blacklist.cfg"
+    with open(blacklist_file, 'r') as file:
+        blacklisted_urls = file.readlines()
+    blacklisted_urls = set(map(lambda x: x.strip(), blacklisted_urls))
+    if new_host in blacklisted_urls:
+        print(f'Requested blacklisted {new_host}, Responded with 403 code')
+        send_block_message(client_socket)
+        return
 
     cache_dir = "cache/"
     cached_file_path = os.path.join(cache_dir, hashlib.md5(url.encode()).hexdigest())
-
     if os.path.exists(cached_file_path):
         with open(cached_file_path, 'rb') as cached_file:
-            response = cached_file.read()  # Читаем ответ из кеша
+            response = cached_file.read()
         print(f"Serving from cache: {cached_file_path}")
     else:
-        new_request, new_host = transformRequest(request, host, "GET", url, protocol)
-
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.connect((new_host, 80))
 
@@ -94,6 +112,7 @@ def proxy_server(client_socket, addr):
     client_socket.sendall(response)
 
     client_socket.close()
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
